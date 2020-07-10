e631b2a7297237c82fab2735f2bb66e3ab494a16
Jwilson1172 <jw59615@gmail.com>
2020-07-10 00:21:01 -0400

    word_count first pass

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,18 @@
 def word_count(s):
-    # Your code here
-
+    ignored = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(' ')
+    # removing all of the puncuation by removing them with the string's replace
+    # function
+    for i in ignored:
+        s.replace(i, '')
+    s = s.split(' ')
+    return s
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count(
+            'This is a test of the emergency broadcast network. This is only a test.'
+        ))
