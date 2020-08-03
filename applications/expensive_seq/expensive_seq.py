45b52bee9b3659d9ca519b4069ac6dfde41b6714
Jwilson1172 <jw59615@gmail.com>
2020-08-03 16:11:13 -0400

    updating exps

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,20 @@
-# Your code here
-
-
+cache = {}
 def expensive_seq(x, y, z):
-    # Your code here
-
+    # check if we can return a cached version
+    if (x, y, z) in cache:
+        return cache[(x, y, z)]
+    # if not then we are going to have to compute the function
+    else:
+        if x <= 0:
+            cache[(x, y, z)] = y + z
+        if x >  0:
+            cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(50):
         x = expensive_seq(i*2, i*3, i*4)
-        print(f"{i*2} {i*3} {i*4} = {x}")
+        print(f"count : {i} | {i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
